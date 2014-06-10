023177b326da12c8ac4f9a859b4b8c584050669f
Mike Barkas <mike@barkas.com>
2014-06-09 21:15:24 -0400

    Continued development.

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <html>
-<head>
 
+<head>
+<?php include 'inc/meta/meta.php'; ?>
+<link rel="stylesheet" type="text/css" href="css/global.css">
 </head>
+
 <body>
 <div id="container">
 
+<div id="header">
+	<?php include 'inc/structure/header.php'; ?>
+</div>
 
+	<div id="main-nav">
+		<?php include 'inc/nav/main-nav.php'; ?>
+	</div>
 
+	<div id="content">
+		<?php include 'inc/content/content.php'; ?>
+	</div>
 
-
-
+	<div id="footer">
+		<?php include 'inc/structure/footer.php'; ?>
+	</div>
 
 </div>
 </body>
